c2485bccc130f5be0480b1f11365ced9e3a1d0a7
ThomasUcll <99878222+ThomasUcll@users.noreply.github.com>
2023-03-26 12:34:29 +0200

    setup of the plugin in the example state

--- src/main/java/org/flurl/CapeLengthConfig.java
+++ src/main/java/org/flurl/CapeLengthConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package org.flurl;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface CapeLengthConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/test/java/capeLength/CapeLengthPluginTest.java
+++ src/test/java/capeLength/CapeLengthPluginTest.java
@@ -1,13 +1,14 @@
-package com.example;
+package capeLength;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
+import org.flurl.CapeLengthPlugin;
 
-public class ExamplePluginTest
+public class CapeLengthPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(CapeLengthPlugin.class);
 		RuneLite.main(args);
 	}
 }
